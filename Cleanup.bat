211faaab5c39c8ac1d8652ae076ae157a6ada526
laukks2000 <106789488+laukks2000@users.noreply.github.com>
2022-09-30 20:32:19 +0800

    Update Cleanup.bat

--- Cleanup.bat
+++ Cleanup.bat
@@ -1,20 +1,19 @@
 @echo off
 
-if exist "C:\Users\Kenneth\AppData\Local\Cache" (
-	rmdir "C:\Users\Kenneth\AppData\Local\Cache" /s /q
+if exist "C:\Users\Username\AppData\Local\Cache" (
+	rmdir "C:\Users\Username\AppData\Local\Cache" /s /q
 )
 
-if exist "C:\Users\Kenneth\AppData\Local\CrashDumps" (
-	rmdir "C:\Users\Kenneth\AppData\Local\CrashDumps" /s /q
+if exist "C:\Users\Username\AppData\Local\CrashDumps" (
+	rmdir "C:\Users\Username\AppData\Local\CrashDumps" /s /q
 )
 
-if exist "C:\Users\Kenneth\AppData\Local\D3DSCache" (
-	rmdir "C:\Users\Kenneth\AppData\Local\D3DSCache" /s /q
+if exist "C:\Users\Username\AppData\Local\D3DSCache" (
+	rmdir "C:\Users\Username\AppData\Local\D3DSCache" /s /q
 )
 
-
-if exist "C:\Users\Kenneth\AppData\Local\Temp" (
-	rmdir "C:\Users\Kenneth\AppData\Local\Temp" /s /q
+if exist "C:\Users\Username\AppData\Local\Temp" (
+	rmdir "C:\Users\Username\AppData\Local\Temp" /s /q
 )
 
 pause
